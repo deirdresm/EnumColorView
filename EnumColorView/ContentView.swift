1525742851d7ff6253e84add24c23ddfd6266947
Deirdre Saoirse Moen <deirdre@deirdre.net>
2023-09-28 12:46:48 -0700

    Initial Commit

---
+++ EnumColorView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnumColorView
+//
+//  Created by Deirdre Saoirse Moen on 9/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnumColorView/EnumColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumColorViewApp.swift
+//  EnumColorView
+//
+//  Created by Deirdre Saoirse Moen on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct EnumColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
